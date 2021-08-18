04f426993a1ec74316e19c2101082c84a0484cd1
Shane Dalton <velmoria@hotmail.com>
2021-08-18 12:05:08 +1000

    project push

---
+++ Source/KidRedGame/KidRedGame.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KidRedGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KidRedGame, "KidRedGame" );
+ 
