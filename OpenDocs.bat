c05d8bc2cd812c9ebeeb429296bee9bcb484eb76
mastercoms <mastercoms@tuta.io>
2021-08-20 05:00:51 -0400

    change port and name

--- OpenDocs.bat
+++ OpenDocs.bat
@@ -2,12 +2,12 @@
 tasklist /FI "WINDOWTITLE eq Docs server" 2>NUL | find /I /N "caddy.exe">NUL
 set found="%ERRORLEVEL%"
 if NOT %found%=="0" (
-	start /MIN "Docs server" ".github\caddy\caddy.exe" run --config .github/caddy/Caddyfile
+	start /MIN "KR Docs server" ".github\caddy\caddy.exe" run --config .github/caddy/Caddyfile
 )
-start http://localhost:2004
+start http://localhost:2005
 if NOT %found%=="0" (
 	color 06
 	echo Docs started. Press any key to exit...
 	pause >> nul
-	taskkill /F /FI "WINDOWTITLE eq Docs server" >nul 2>&1
+	taskkill /F /FI "WINDOWTITLE eq KR Docs server" >nul 2>&1
 )
