2f7b74eb3396ba5604d3a861cf6602d478ab2c7d
mastercoms <mastercoms@tuta.io>
2021-09-26 05:03:13 +0000

    fix more update bugs

--- UpdateProject.bat
+++ UpdateProject.bat
@@ -5,16 +5,17 @@ CALL :CHECK_FAIL
 for /f %%w in ('git rev-parse --abbrev-ref HEAD') do set branch=%%w
 git fetch --no-tags origin %branch%
 CALL :CHECK_FAIL
-git restore -qWSs origin/%branch% -- PBSync.exe PBSync.xml
+git restore -qWs origin/%branch% -- PBSync.exe PBSync.xml
 CALL :CHECK_FAIL
-IF EXIST "PBSyncTemp" rd PBSyncTemp /s /q
+IF EXIST "PBSyncTemp" rd /s /q PBSyncTemp
 CALL :CHECK_FAIL
 robocopy /nfl /njh /njs /copy:D /MT:2 /r:3 /w:1 . PBSyncTemp PBSync.exe PBSync.xml
 if NOT ["%errorlevel%"]==["1"] (
     pause
     exit %errorlevel%
 )
-git restore -qWSs HEAD PBSync.exe PBSync.xml
+git restore -q -- PBSync.exe PBSync.xml
+git update-index -- PBSync.exe PBSync.xml
 CALL :CHECK_FAIL
 PBSyncTemp\PBSync.exe --config PBSyncTemp/PBSync.xml --sync all
 CALL :CHECK_FAIL
